8c92d158d46c15f31e26dba74eddf7e7f3ca4b8c
danielvivacqua <drvivacqua@hotmail.com>
2017-07-10 10:27:52 -0400

    Created first method Add

--- MethodsPracticeSummer17/Program.cs
+++ MethodsPracticeSummer17/Program.cs
@@ -10,6 +10,48 @@ namespace MethodsPracticeSummer17
     {
         static void Main(string[] args)
         {
+            //Methods are a tool for us to create reusable pieces of code.
+
+            //Methods are a way of us collecting a series of instructions and then call them
+            //when we need them.
+
+            //Methods are ALWAYS part of a class. Methods are ALWAYS children of classes.
+            //This means a method will NEVER be created inside another method or member of 
+            //the class. However, methods are frequently CALLED inside of another class or method.
+
+            Console.WriteLine("Hello. Please enter the first number to be added.");
+            int numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number to be added.");
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            int answer = Add(numberOne, numberTwo);
+
+            Console.WriteLine("The sum of your two numbers is " + answer);
+
+
+
+
+
+
+
         }
+
+        //Method header
+        //Access Modifier - Return Type - Method Name (in Pascal Case) - Parentheses (sometimes with
+        //parameters)
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            //Method body
+            //A complete method - header and body - is called a method declaration
+
+            int sum = firstNumber + secondNumber;
+
+            return sum;
+            //The "return" keyword takes whatever value is determined by using this method
+            //and sends that value back to where I call my method
+        }
+        //If you have a method that does not have a return value, you would use the keyword
+        //"void". When we have a "void" return type we DON'T need to use the keyword "return"
+
     }
 }
